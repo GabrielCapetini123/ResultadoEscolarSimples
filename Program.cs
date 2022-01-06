7396f105e11a6d59e9608df147103a52bd75a365
GabrielCapetini123 <87335853+GabrielCapetini123@users.noreply.github.com>
2022-01-06 12:06:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Relembrando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota: ");
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota: ");
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+        }
+    }
+}
